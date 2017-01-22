0ea3a15f8d02edd869cc591b03543692e68a6b51
Chen Shen <chenshen@fb.com>
2017-01-22 13:34:21 -0800

    working on basics

--- basics.v
+++ basics.v
@@ -109,7 +109,139 @@ Definition pred (n : nat) : nat :=
     | S n' => n'
   end.
 
-End Playground1
+End Playground1.
 
-Check (S O)
+Definition minustwo (n : nat) : nat :=
+  match n with 
+    | O => O
+    | S O => O
+    | S (S n') => n'
+  end.
 
+Check (S (S (S (S O)))).
+
+Compute (minustwo 4).
+
+Check S.
+Check pred.
+Check minustwo.
+
+Fixpoint evenb (n:nat) : bool :=
+  match n with
+    | O => true
+    | S O => false
+    | S (S n') => evenb n'
+  end.
+
+Definition oddb (n:nat) : bool := negb (evenb n).
+
+Example test_oddb1: oddb 1 = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_oddb2: oddb 4 =  false.
+Proof. reflexivity. Qed.
+
+Module Playground2.
+
+Fixpoint plus (n: nat) (m: nat) : nat :=
+  match n with
+  | O => m
+  | S n' => S (plus n' m)
+  end.
+
+Compute (plus 3 2).
+
+Fixpoint mult (n m : nat) : nat :=
+  match n with 
+    | O => O
+    | S n' => plus m (mult n' m)
+  end.
+
+Example test_multi: (mult 3 3) = 9.
+Proof. reflexivity. Qed.
+
+Fixpoint minus (n m : nat) : nat := 
+  match n, m with
+    | O, _ => O
+    | S _, O => n
+    | S n', S m' => minus n' m'
+  end.
+End Playground2.
+
+Fixpoint exp (base power : nat) : nat :=
+  match power with
+    | O => S O
+    | S n => mult base (exp base n)
+  end.
+
+(* Exc factorial *)
+Fixpoint factorial (n:nat) : nat :=
+  match n with
+    | O => S O
+    | S n' => mult n (factorial n')
+  end.
+
+Example test_factorial1: (factorial 3) = 6.
+Proof. simpl. reflexivity. Qed.
+
+Example test_factorial2: (factorial 5) = (mult 10 12).
+Proof. simpl. reflexivity. Qed.
+
+Notation "x + y" := (plus x y)
+  (at level 50, left associativity)
+  : nat_scope.
+
+Notation "x - y" := (minus x y)
+  (at level 50, left associativity)
+  : nat_scope.
+
+Notation "x * y" := (mult x y)
+  (at level 40, left associativity)
+  : nat_scope.
+
+Check ((0 + 1) + 1).
+Example test_notation: ((0 + 1) + 1) = 2.
+Proof. simpl. reflexivity. Qed.
+
+Fixpoint beq_nat (n m : nat) : bool :=
+  match n with
+   | O => match m with
+            | O => true
+            | _ => false
+          end
+   | S n' => match m with
+              | O => false
+              | S m' => beq_nat n' m'
+             end
+  end.
+
+Fixpoint leb (n m: nat): bool := 
+  match n with
+    | O => true
+    | S n' =>
+      match m with
+        | O => false
+        | S m' => leb n' m'
+      end
+  end.
+
+Example test_leb1: (leb 2 2) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_leb2: (leb 2 4) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_leb3: (leb 4 2) = false.
+Proof. simpl. reflexivity. Qed.
+
+(* Exc blt_nat *)
+Definition blt_nat (n m: nat) : bool :=
+  match (beq_nat n m) with
+    | true => false
+    | false => leb n m
+  end.
+
+Example test_blt_nat1: (blt_nat 2 2) = false.
+Proof. simpl. reflexivity. Qed.
+Example test_blt_nat2: (blt_nat 2 4) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_blt_nat3: (blt_nat 4 2) = false.
+Proof. simpl. reflexivity. Qed.
