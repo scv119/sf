e407dbadf77ac98850eb72879725b7d7191c3a19
Chen Shen <chenshen@fb.com>
2017-01-30 22:57:33 -0800

    finish options

--- Poly.v
+++ Poly.v
@@ -97,3 +97,99 @@ Notation "[ x ; .. ; y ]" := (cons x .. (cons y []) ..).
 Notation "x ++ y" := (app x y)
                      (at level 60, right associativity).
 
+Theorem app_nil_r : forall X : Type, forall l : list X,
+  l ++ [] = l.
+Proof.
+  intros X l. induction l as [|x l' IHl'].
+  - reflexivity.
+  - simpl. rewrite -> IHl'. reflexivity. Qed.
+
+Theorem app_assoc: forall A (l m n : list A),
+  l ++ m ++ n = (l ++ m ) ++ n.
+Proof.
+  intros A l m n. induction l as [|a l' IHl'].
+  - reflexivity.
+  - simpl. rewrite -> IHl'. reflexivity. Qed.
+
+Theorem app_length : forall (X : Type) (l1 l2 : list X),
+  length (l1 ++ l2) = length l1 + length l2.
+Proof.
+  intros X l1 l2. induction l1 as [|x l1' IHl1'].
+  - reflexivity.
+  - simpl. rewrite -> IHl1'. reflexivity. Qed.
+
+Inductive prod (X Y : Type) : Type :=
+| pair : X -> Y -> prod X Y.
+
+Arguments pair {X} {Y} _ _.
+Notation "( x , y )" := (pair x y). (* A value *)
+Notation "X * Y":= (prod X Y) : type_scope. (* A type *)
+
+Definition fst {X Y : Type} (p : prod X Y ) : X :=
+  match p with
+  | (x, y) => x
+  end.
+
+Definition snd {X Y : Type} (p : X * Y) : Y :=
+  match p with
+  | (x ,y) => y
+  end.
+
+Fixpoint combine {X Y : Type} (lx : list X) (ly : list Y) : list (X*Y) :=
+  match lx, ly with
+  | [], _ => []
+  | _, [] => []
+  | x :: tx, y :: ty => (x, y) :: (combine tx ty)
+  end.
+
+Check @nil.
+(* Forall X: Type Forall Y: Type, list X -> list Y -> list X*Y *)
+Check @combine.
+
+(* [(1, false);(2, false)] *)
+Compute (combine [1;2] [false;false;true;true]).
+
+Fixpoint split {X Y : Type} (l: list (X*Y)) : (list X) * (list Y) :=
+  match l with
+  | [] => ([], [])
+  | (x, y) :: l' => match split l' with 
+                    | (lx, ly) => (x::lx, y::ly)
+                    end
+  end.
+
+Example test_split:
+  split [(1,false);(2,false)] = ([1;2],[false;false]).
+Proof. reflexivity. Qed.
+
+Inductive option (X : Type) : Type :=
+  | Some: X -> option X
+  | None: option X.
+
+Arguments Some {X} _.
+Arguments None {X}.
+
+Fixpoint nth_error {X : Type} (l : list X) (n : nat) : option X :=
+  match l with
+  | nil => None
+  | a :: l' => if beq_nat n 0 then Some a else nth_error l' (pred n)
+  end.
+
+Example test_nth_error1 : nth_error [4;5;6;7] 0 = Some 4.
+Proof. reflexivity. Qed.
+Example test_nth_error2 : nth_error [[1];[2]] 1 = Some [2].
+Proof. reflexivity. Qed.
+Example test_nth_error3 : nth_error [true] 2 = None.
+Proof. reflexivity. Qed.
+
+
+Definition hd_error {X : Type} (l : list X) : option X :=
+  match l with
+  | nil => None
+  | x :: _ => Some x
+  end.
+
+Check @hd_error.
+Example test_hd_error1 : hd_error [1;2] = Some 1.
+Proof. reflexivity. Qed.
+Example test_hd_error2 : hd_error [[1];[2]] = Some [1].
+Proof. reflexivity. Qed.
