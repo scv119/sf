c3e628d9ae2c54d21d157ddae786c5bf242b7286
Chen Shen <chenshen@fb.com>
2017-01-30 22:21:27 -0800

    work on poly

--- Poly.v
+++ Poly.v
@@ -22,7 +22,78 @@ Fixpoint repeat (X : Type) (x : X) (count : nat) : list X :=
 
 Module MumbleGrumble.
 
-Inductive mumble : Type : =
+Inductive mumble : Type :=
   | a : mumble
   | b : mumble -> nat -> mumble
-  | c : mumblen
+  | c : mumble.
+
+Inductive grumble (X:Type) : Type :=
+  | d : mumble -> grumble X
+  | e : X -> grumble X.
+
+(* Check d (b a 5). *)
+Check d mumble (b a 5).
+Check d bool (b a 5).
+Check e bool true.
+Check e mumble (b c 0).
+(* Check e bool (b c 0). *)
+Check c.
+Check d bool c.
+
+End MumbleGrumble.
+
+(* Type Inference *)
+Fixpoint repeat' X x count : list X :=
+  match count with
+  | 0 => nil X
+  | S count' => cons X x (repeat' X x count')
+  end.
+
+(* Type Arugment Synthesis *)
+
+Definition list123' :=
+  cons _ 1 (cons _ 2 (cons _ 3 (nil _))).
+
+(* Implicit Arguments  *)
+Arguments nil {X}.
+Arguments cons {X} _ _.
+Arguments repeat {X} x count.
+
+Definition list123'' := cons 1 (cons 2( cons 3 nil)). 
+
+Fixpoint repeat''' {X : Type} (x : X) (count : nat) : list X :=
+  match count with
+  | 0 => nil
+  | S count' => cons x (repeat''' x count')
+  end.
+
+Fixpoint app {X:Type} (l1 l2 : list X) : list X :=
+  match l1 with
+  | nil => l2
+  | cons h t => cons h (app t l2)
+  end.
+
+Fixpoint rev {X:Type} (l: list X) : list X :=
+  match l with
+  | nil => nil
+  | cons h t => app (rev t) (cons h nil)
+  end.
+
+Fixpoint length {X : Type} (l : list X) : nat :=
+  match l with
+  | nil => 0
+  | cons _ l' => S (length l')
+  end.
+
+Fail Definition mynil := nil.
+
+Definition mynil : list nat := nil.
+Definition mynil' := @nil nat.
+
+Notation "x :: y" := (cons x y)
+                     (at level 60, right associativity).
+Notation "[ ]" := nil.
+Notation "[ x ; .. ; y ]" := (cons x .. (cons y []) ..).
+Notation "x ++ y" := (app x y)
+                     (at level 60, right associativity).
+
