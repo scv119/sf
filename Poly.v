d25b46263aa68b3c57add5ddce9d89d700fbe678
Chen Shen <chenshen@fb.com>
2017-02-01 20:49:20 -0800

    map and more

--- Poly.v
+++ Poly.v
@@ -1,4 +1,4 @@
-Add LoadPath "/Users/chenshen/src/sf/".
++Add LoadPath "/Users/chenshen/src/sf/".
 Require Export Lists.
 
 Inductive boollist : Type :=
@@ -256,3 +256,182 @@ Example test_partition1: partition oddb [1;2;3;4;5] = ([1;3;5], [2;4]).
 Proof. reflexivity. Qed.
 Example test_partition2: partition (fun x => false) [5;9;0] = ([], [5;9;0]).
 Proof. reflexivity. Qed.
+
+Fixpoint map {X Y : Type} (f : X -> Y) (l : list X) : (list Y) :=
+  match l with
+  | [] => []
+  | h :: t => (f h) :: (map f t)
+  end.
+
+Theorem map_assoc : forall (X Y: Type) (f : X -> Y) (a b : list X),
+  map f (a ++ b) = map f a ++ map f b.
+Proof.
+  intros X Y f a b. induction a as [|n c IHc].
+  - reflexivity.
+  - simpl. rewrite -> IHc. reflexivity. Qed.
+
+Theorem map_rev : forall (X Y : Type) (f : X -> Y) (l : list X),
+  map f ( rev l) = rev (map f l).
+Proof.
+  intros X Y f l. induction l as [| n l' IHl'].
+  - reflexivity.
+  - simpl. rewrite <- IHl', map_assoc. reflexivity. Qed.
+
+Fixpoint flat_map {X Y:Type} (f:X -> list Y) (l:list X) : (list Y) :=
+  match l with 
+  | [] => []
+  | s :: t => f s ++ flat_map f t
+  end.
+
+Example test_flat_map1:
+  flat_map (fun n => [n;n;n]) [1;5;4]
+  = [1; 1; 1; 5; 5; 5; 4; 4; 4].
+Proof. reflexivity. Qed.
+
+Fixpoint fold {X Y:Type} (f: X -> Y -> Y) (l:list X) (b:Y)
+                         : Y :=
+  match l with
+  | nil => b
+  | h :: t => f h (fold f t b)
+  end.
+
+Definition constfun {X : Type} (x : X) : nat -> X :=
+  fun (k : nat) => x.
+
+Module Exercises.
+
+Definition fold_length {X : Type} (l : list X) : nat :=
+  fold (fun _ n => S n) l 0.
+
+Example test_fold_length1 : fold_length [4;7;0] = 3.
+Proof. reflexivity. Qed.
+
+Theorem fold_length_correct : forall (X :Type) (l : list X),
+  fold_length l = length l.
+Proof. intros X l. induction l as [| n l' IHl'].
+  - reflexivity.
+  - simpl. rewrite <- IHl'. reflexivity. Qed.
+
+Definition fold_map {X Y:Type} (f : X -> Y) (l: list X) : list Y :=
+  fold (fun s t => (f s) :: t) l [].
+
+Theorem map_fold_correct : forall (X Y: Type) (f : X -> Y) (l : list X),
+  map f l = fold_map f l.
+Proof. intros X Y f l. induction l as [|n l' IHl'].
+  - reflexivity.
+  - simpl. rewrite -> IHl'. reflexivity. Qed.
+
+Definition prod_curry {X Y Z: Type}
+  (f : X * Y -> Z) (x : X) (y : Y) : Z := f (x, y).
+
+Definition prod_uncurry {X Y Z: Type}
+  (f : X -> Y -> Z) (p : X * Y) : Z :=
+  match p with
+  | (x, y) => f x y
+  end.
+
+Check @prod_curry.
+Check @prod_uncurry.
+
+Theorem uncurry_curry : forall (X Y Z : Type) (f : X -> Y -> Z) x y,
+  prod_curry (prod_uncurry f) x y = f x y.
+Proof.
+  intros X Y Z f x y. reflexivity. Qed.
+
+
+Theorem curry_uncurry : forall (X Y Z : Type) (f : (X * Y) -> Z) (p : X * Y),
+  prod_uncurry (prod_curry f) p = f p.
+Proof.
+  intros X Y Z f p.  destruct p as [x y].
+  - reflexivity. Qed.
+
+Theorem nth_error_length :  forall (X : Type) (l : list X),
+  nth_error l (length l) = None.
+Proof. intros X l. induction l as [| n l' IHl'].
+  - reflexivity.
+  - simpl. rewrite -> IHl'. reflexivity. Qed.
+
+Theorem nth_error_length_none : forall (X : Type) (n : nat) (l : list X),
+  length l = n -> nth_error l n = None.
+Proof.
+  intros X n l H. rewrite <- H. rewrite -> nth_error_length. reflexivity. Qed.
+
+Module Church.
+Definition nat := forall (X : Type), (X -> X) -> X -> X.
+Definition one : nat :=
+  fun (X : Type) (f: X -> X) (x : X) => f x.
+Definition two : nat :=
+  fun (X : Type) (f: X -> X) (x : X) => f (f x).
+Definition zero : nat :=
+  fun (X : Type) (f: X -> X) (x : X) => x.
+Definition three : nat := @doit3times.
+
+
+Definition succ (n : nat) : nat :=
+  fun (X : Type) (f: X -> X) (x : X) => f (n X f x).
+
+Example succ_1 : succ zero = one.
+Proof. reflexivity. Qed.
+
+Example succ_2 : succ one = two.
+Proof. reflexivity. Qed.
+
+Example succ_3 : succ two = three.
+Proof. reflexivity. Qed.
+
+Definition plus (n m : nat) : nat :=
+  fun (X : Type) (f: X -> X) (x : X) => m X f (n X f x).
+
+Example plus_1 : plus zero one = one.
+Proof. reflexivity. Qed.
+
+Example plus_2 : plus two three = plus three two.
+Proof. reflexivity. Qed.
+
+Example plus_3 :
+  plus (plus two two) three = plus one (plus three three).
+Proof. reflexivity. Qed.
+
+Definition mult (n m : nat) : nat :=
+  fun (X : Type) (f: X -> X) (x : X) => (n X (m X f)) x.
+
+Example mult_1 : mult one one = one.
+Proof. reflexivity. Qed.
+
+Example mult_2 : mult zero (plus three three) = zero.
+Proof. reflexivity. Qed.
+
+Example mult_3 : mult two three = plus three three.
+Proof. reflexivity. Qed.
+
+(* By observate that 
+
+(multi m m) f = m (m f)
+
+we could see 
+
+exp n m f = n (n ( n ... f))
+
+So we should apply m to n, the only problem is type.
+
+Given n has type X : (X -> X) -> X -> X
+for m we have   Y : (Y -> Y) -> Y -> Y, 
+wehere Y -> Y  == (X -> X) -> (X -> X)
+we could see Y = ( X -> X).
+*)
+
+Definition exp (n m : nat) : nat := 
+  fun (X : Type) (f: X -> X) (x : X) => (m _ (n X)) f x.
+
+Example exp_1 : exp two two = plus two two.
+Proof. reflexivity. Qed.
+
+Example exp_2 : exp three two = plus (mult two (mult two two)) one.
+Proof. reflexivity. Qed.
+
+Example exp_3 : exp three zero = one.
+Proof. reflexivity. Qed.
+
+End Church.
+
+End Exercises.
