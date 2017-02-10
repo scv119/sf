3c6d45378e2e1370da7a3fd00ac4451d87da949d
Chen Shen <chenshen@fb.com>
2017-02-09 23:27:19 -0800

    logics

--- Logic.v
+++ Logic.v
@@ -188,3 +188,147 @@ Qed.
 Lemma True_is_true : True.
 Proof. apply I. Qed.
 
+Module MyIff.
+
+Definition iff (P Q : Prop) := (P -> Q) /\ (Q -> P).
+Notation " P <-> Q " := (iff P Q)
+                        (at level 95, no associativity)
+                        : type_scope.
+
+End MyIff.
+
+Theorem iff_sym : forall P Q : Prop,
+  (P <-> Q) -> (Q <-> P).
+Proof.
+  intros P Q H. unfold iff in H. destruct H as [HA HB]. unfold iff.
+  split.
+  - apply HB.
+  - apply HA.
+Qed.
+
+Lemma not_true_iff_false : forall b,
+  b <> true <-> b = false.
+Proof.
+  intros b. split.
+  - apply not_true_is_false.
+  - intros H. rewrite H. unfold not. intros H'. inversion H'.
+Qed.
+
+
+Theorem iff_refl : forall P : Prop,
+  P <-> P.
+Proof.
+  intros P. split.
+  - intros H. apply H.
+  - intros H. apply H.
+Qed.
+
+Theorem iff_trans : forall P Q R : Prop,
+  (P <-> Q) -> (Q <-> R) -> (P <-> R).
+Proof.
+  intros P Q R [H1 H2] [H3 H4]. split.
+  - intros H5. apply H3. apply H1. apply H5.
+  - intros H5. apply H2. apply H4. apply H5.
+Qed.
+
+Theorem or_distributes_over_and : forall P Q R : Prop,
+  P \/ (Q /\ R ) <-> (P \/ Q) /\ (P \/ R).
+Proof.
+  intros P Q R. split.
+  - intros [H1 | [H3 H4]].
+   + split.
+    * left. apply H1.
+    * left. apply H1.
+   + split.
+    * right. apply H3.
+    * right. apply H4.
+  - intros [[H1|H2] [H3|H4]].
+    * left. apply H1.
+    * left. apply H1.
+    * left. apply H3.
+    * right. split. apply H2. apply H4.
+Qed.
+
+Require Import Coq.Setoids.Setoid.
+
+
+Lemma or_example :
+  forall n m : nat, n = 0 \/ m = 0 -> n * m = 0.
+Proof.
+  (* This pattern implicitly does case analysis on
+     n = 0 ∨ m = 0 *)
+  intros n m [Hn | Hm].
+  - (* Here, n = 0 *)
+    rewrite Hn. reflexivity.
+  - (* Here, m = 0 *)
+    rewrite Hm. rewrite <- mult_n_O.
+    reflexivity.
+Qed.
+
+Lemma mult_0 : forall n m, n * m = 0 <-> n = 0 \/ m = 0.
+Proof.
+  split.
+  - apply mult_eq_0.
+  - apply or_example.
+Qed.
+
+Lemma or_assoc :
+  forall P Q R : Prop, P \/ (Q \/ R) <-> (P \/ Q) \/ R.
+Proof.
+  intros P Q R. split.
+  - intros [H | [H | H]].
+    + left. left. apply H.
+    + left. right. apply H.
+    + right. apply H.
+  - intros [[H | H] | H].
+    + left. apply H.
+    + right. left. apply H.
+    + right. right. apply H.
+Qed.
+
+Lemma mult_0_3 :
+  forall n m p, n * m * p = 0 <-> n = 0 \/ m = 0 \/ p = 0.
+Proof.
+  intros n m p.
+  rewrite mult_0. rewrite mult_0. rewrite or_assoc.
+  reflexivity.
+Qed.
+
+Lemma apply_iff_example :
+ forall n m : nat, n * m = 0 -> n = 0 \/ m = 0.
+Proof.
+  intros n m H. apply mult_0. apply H.
+Qed.
+
+
+Lemma four_is_even : exists n : nat, 4 = n + n.
+Proof.
+  exists 2. reflexivity.
+Qed.
+
+Theorem exists_example_2 : forall n,
+  (exists m, n = 4 + m) ->
+  (exists o, n = 2 + o).
+Proof.
+  intros n [m Hm].
+  exists (2 + m).
+  apply Hm. Qed.
+
+Theorem dist_not_exists : forall (X : Type) (P : X -> Prop),
+  (forall x, P x) -> ~ (exists x, ~ P x).
+Proof.
+  intros X P H. unfold not. intros H1. inversion H1 as [x Hx]. apply Hx in H. apply H.
+Qed.
+
+Theorem dist_exists_or : forall (X:Type) (P Q : X -> Prop),
+  (exists x, P x \/ Q x) <-> (exists x, P x) \/ (exists x, Q x).
+Proof.
+  intros X P Q. split.
+  - intros [x [H1 | H2]].
+    + left. exists x. apply H1.
+    + right. exists x. apply H2.
+  - intros [[x H] | [x H]].
+    + exists x. left. apply H.
+    + exists x. right. apply H.
+Qed.
+
